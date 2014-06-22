dd19940b5c7f34f58b2cfd077301e2dbeb4b9b4e
Root <root@zaraza.lt>
2014-06-22 13:52:29 +0300

    Resolver updated

--- lib/Levelup/Framework/Controller/ControllerResolver.php
+++ lib/Levelup/Framework/Controller/ControllerResolver.php
@@ -21,13 +21,22 @@ class ControllerResolver extends BaseControllerResolver
 
     protected function createController($controller)
     {
-        $count = substr_count($controller, ':');
-        if (1 == $count) {
-            // controller in the service:method notation
-            list($class, $method) = explode(':', $controller, 2);
-        } else {
-            throw new \LogicException(sprintf('Unable to parse the controller name "%s".', $controller));
+        if (false === strpos($controller, '::')) {
+            $count = substr_count($controller, ':');
+            if (2 == $count) {
+                // controller in the a:b:c notation then
+                $controller = $this->parser->parse($controller);
+            } elseif (1 == $count) {
+                // controller in the service:method notation
+                list($service, $method) = explode(':', $controller, 2);
+
+                return array($this->container->get($service), $method);
+            } else {
+                throw new \LogicException(sprintf('Unable to parse the controller name "%s".', $controller));
+            }
         }
+
+        list($class, $method) = explode('::', $controller, 2);
 
         if (!class_exists($class)) {
             throw new \InvalidArgumentException(sprintf('Class "%s" does not exist.', $class));
